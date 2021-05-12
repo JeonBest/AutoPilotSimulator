54b3124b6cc6ba4126fd16c7148c65e8de874101
JeonBest <JeonBest@github.com>
2021-05-13 06:07:58 +0900

    차로중앙유지, 크루즈컨트롤

--- Driving Simulator/Assets/MyFolder/GoodDriverAI.cs
+++ Driving Simulator/Assets/MyFolder/GoodDriverAI.cs
@@ -9,106 +9,100 @@ namespace NWH.VehiclePhysics2.Input
     {
         [Header ("Common")]
         public VehicleController myvehicle;
-        public GameObject rayPivot;
-        public GameObject FrontSensor;
+        public GameObject Track;
+        public Color lineColor;
 
         [Header ("AI settings")]
         public float targetspeed;
+        public float firstMinPivotDis;
         public float steeringCoefficient;
 
         [Header ("Only for Read")]
         public float steeringValue;
+        public float minPivotDis;
 
-        private RaycastHit hit;
-        private float HitdisR;
-        private float HitdisL;
-        private float LaneMaxDistance;
-        private int LaneMask;
-        private GameObject FrontCar;
+        VehicleController FrontCar;
+        GuidePivotManager.GuidePivot currentPivot;
+        GuidePivotManager GPM;
+        Modules.CruiseControl.CruiseControlModule cruiseControl;
+        private bool isEngineStart = false;
 
         // Start is called before the first frame update
         void Start()
         {
-            myvehicle.input.autoSetInput = false;
-            Invoke("EngineStart", 3.0f);
-            Invoke("RaceStart", 2.0f);
-            HitdisR = 5f;
-            HitdisL = 5f;
-        }
+            myvehicle.input.autoSetInput = true;
+            minPivotDis = firstMinPivotDis;
 
-        void EngineStart()
-        {
-            myvehicle.input.EngineStartStop = true;
+            Invoke("EngineStart", 4.0f);
+            Invoke("RaceStart", 2.0f);
         }
 
         void RaceStart()
         {
-            myvehicle.input.TrailerAttachDetach = true;
+            cruiseControl = myvehicle.moduleManager.GetModule<Modules.CruiseControl.CruiseControlModule>();
+
+            /* 자신과 가장 가까운 GuidePivot 찾기 */
+            GPM = Track.GetComponent<GuidePivotManager>();
+            float minimunDis = 100f;
+            foreach (GuidePivotManager.GuidePivot gp in GPM.guideLine)
+            {
+                float distance = (myvehicle.vehicleTransform.position - gp.cur.position).sqrMagnitude;
+                if (minimunDis > distance)
+                {
+                    minimunDis = distance;
+                    currentPivot = gp;
+                }
+            }
+
+            // myvehicle.input.TrailerAttachDetach = true;
+
+            cruiseControl.setTargetSpeedOnEnable = false;
+            cruiseControl.targetSpeed = targetspeed;
+            
+            
+        }
+
+        void EngineStart()
+        {
+            cruiseControl.Enable();
+            isEngineStart = true;
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-            // Front Sensor에 앞 차가 감지되었는지 확인
-            if (!VFS.isFree2Go)
-            {
-                // Sensor 범위에 차가 있다면, 액셀값 0
-                myvehicle.input.Throttle = 0f;
+            if (!isEngineStart)
+                return;
 
-                FrontCar = VFS.FrontVehicle;
-                float FrontCarDistance = Vector3.Distance(myvehicle.vehicleTransform.position, FrontCar.transform.position);
-                float FrontCarSpeed = FrontCar.GetComponent<Rigidbody>().velocity.magnitude;
-                Debug.Log("Front Car Distance : " + FrontCarDistance);
-                
-                // 감지된 차보다 내 차가 더 빠르면, 브레이크
-                if (myvehicle.VelocityMagnitude > FrontCarSpeed)
-                {
-                    // 감지된 차와의 거리가 10m보다 가깝다면, 급정거
-                    if (FrontCarDistance < 12f)
-                    {
-                        myvehicle.input.Handbrake = 1f;
-                        myvehicle.input.Brakes = myvehicle.LocalForwardVelocity < 1f ? 0f : 1f;
-                        Debug.Log("close local forward velocity : " + myvehicle.LocalForwardVelocity);
-                    }
-                    // 10m보다 멀다면, 거리에 따라 브레이킹
-                    else
-                    {
-                        myvehicle.input.Handbrake = 0f;
-                        myvehicle.input.Brakes = myvehicle.LocalForwardVelocity < 1f ? 0f : 1f / (FrontCarDistance - 11f);
-                        Debug.Log("far local forward velocity : " + myvehicle.LocalForwardVelocity);
-                    }
-                }
-                // 감지된 차보다 내 차가 더 느리면, 감지범위 밖에 나갈 때까지 액셀값 0
-                else
-                {
-                    myvehicle.input.Handbrake = 0f;
-                }
-            }
-            else 
-            {
-                myvehicle.input.Brakes = 0f;
-                myvehicle.input.Handbrake = 0f;
-                myvehicle.input.Throttle = (targetspeed - myvehicle.VelocityMagnitude / 10f) > 0 ?
-                    (targetspeed - myvehicle.VelocityMagnitude / 10f) : 0;
-            }
+            /* 속도에 따라 minPivotDis 조절 */
+            if (myvehicle.LocalForwardVelocity > 24)
+                minPivotDis = myvehicle.LocalForwardVelocity;
+            else if (myvehicle.LocalForwardVelocity > 18)
+                minPivotDis = 12;
+            else
+                minPivotDis = firstMinPivotDis;
 
-            // 오른쪽 차선과의 거리 확인
-            if (Physics.Raycast(rayPivot.transform.position, rayPivot.transform.right, out hit, LaneMaxDistance, LaneMask))
-            {
-                HitdisR = hit.distance;
-                Debug.DrawRay(rayPivot.transform.position, rayPivot.transform.right * hit.distance, Color.red);
-                Debug.Log("right collider name : " + hit.collider.gameObject.name);
-            }
-            // 왼쪽 차선과의 거리 확인
-            if (Physics.Raycast(rayPivot.transform.position, rayPivot.transform.right * -1, out hit, LaneMaxDistance, LaneMask))
-            {
-                HitdisL = hit.distance;
-                Debug.DrawRay(rayPivot.transform.position, rayPivot.transform.right * -hit.distance, Color.blue);
-                Debug.Log("left collider name : " + hit.collider.gameObject.name);
-            }
+            /* currentPivot과의 거리가 minPivotDis보다 작아지면, next로 갱신 */
+            if (Vector3.Distance(currentPivot.cur.position, myvehicle.vehicleTransform.position) < minPivotDis)
+                currentPivot = currentPivot.next;
+
+            /* 속도 조절 */
+            cruiseControl.targetSpeed = targetspeed;
+
             // 차선간의 거리의 차가 작도록 핸들조작, 차로중앙유지
-            steeringValue = (HitdisR - HitdisL) / steeringCoefficient;
-            myvehicle.input.Steering = steeringValue;
+            Vector3 relativeVector = myvehicle.vehicleTransform.InverseTransformPoint(currentPivot.cur.position);
+            steeringValue = relativeVector.x / relativeVector.magnitude;
+            myvehicle.input.Steering = steeringValue * steeringCoefficient;
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!isEngineStart)
+                return;
+            Gizmos.color = lineColor;
+            Gizmos.DrawSphere(currentPivot.cur.position, 1f);
+            Gizmos.DrawLine(myvehicle.vehicleTransform.position, currentPivot.cur.position);
         }
     }
 }
--- Driving Simulator/Assets/MyFolder/GuidePivotManager.cs
+++ Driving Simulator/Assets/MyFolder/GuidePivotManager.cs
@@ -4,21 +4,180 @@ using UnityEngine;
 
 public class GuidePivotManager : MonoBehaviour
 {
+    public Transform Track;
+    public Color lineColor;
 
+    public List<GuidePivot> guideLine;
 
     public class GuidePivot
     {
+        public Transform cur;
+        public GuidePivot left;
+        public GuidePivot next;
+        public GuidePivot right;
+        public GuidePivot prev;
 
+        public GuidePivot() { }
+
+        public GuidePivot(Transform _cur)
+        {
+            cur = _cur;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        guideLine = new List<GuidePivot>();
+        Transform[] tmp = Track.GetComponentsInChildren<Transform>();
+
+        GuidePivot[] curLine = { null, null, null };
+        GuidePivot[] oldLine = { null, null, null };
+        foreach (Transform pivotTransform in tmp)
+        {
+            if (!pivotTransform.CompareTag("DriveGuide"))
+                continue;
+
+            GuidePivot current = new GuidePivot(pivotTransform);
+            if (pivotTransform.name == "GuidePivot.1")
+            {
+                curLine[0] = current;
+            }
+            if (pivotTransform.name == "GuidePivot.2")
+            {
+                curLine[1] = current;
+            }
+            if (pivotTransform.name == "GuidePivot.3")
+            {
+                curLine[2] = current;
+
+                if (oldLine[0] != null)
+                {
+                    oldLine[0].next = curLine[0];
+                    oldLine[0].right = curLine[1];
+                    curLine[0].prev = oldLine[0];
+                }
+                if (oldLine[1] != null)
+                {
+                    oldLine[1].left = curLine[0];
+                    oldLine[1].next = curLine[1];
+                    oldLine[1].right = curLine[2];
+                    curLine[1].prev = oldLine[1];
+                }
+                if (oldLine[2] != null)
+                {
+                    oldLine[2].left = curLine[1];
+                    oldLine[2].next = curLine[2];
+                    curLine[2].prev = oldLine[2];
+                }
+
+                guideLine.Add(curLine[0]);
+                guideLine.Add(curLine[1]);
+                guideLine.Add(curLine[2]);
+
+                oldLine[0] = curLine[0];
+                oldLine[1] = curLine[1];
+                oldLine[2] = curLine[2];
+            }
+        }
+        int pivotNum = guideLine.Count;
+        guideLine[0].prev = guideLine[pivotNum - 3];
+        guideLine[1].prev = guideLine[pivotNum - 2];
+        guideLine[2].prev = guideLine[pivotNum - 1];
+
+        guideLine[pivotNum - 3].next = guideLine[0];
+        guideLine[pivotNum - 3].right = guideLine[1];
+
+        guideLine[pivotNum - 2].left = guideLine[0];
+        guideLine[pivotNum - 2].next = guideLine[1];
+        guideLine[pivotNum - 2].right = guideLine[2];
+
+        guideLine[pivotNum - 1].left = guideLine[1];
+        guideLine[pivotNum - 1].next = guideLine[2];
+
     }
 
     private void OnDrawGizmosSelected()
     {
+        guideLine = new List<GuidePivot>();
+        Transform[] tmp = Track.GetComponentsInChildren<Transform>();
+
+        GuidePivot[] curLine = { null, null, null };
+        GuidePivot[] oldLine = { null, null, null };
+        foreach (Transform pivotTransform in tmp)
+        {
+            if (!pivotTransform.CompareTag("DriveGuide"))
+                continue;
+
+            GuidePivot current = new GuidePivot(pivotTransform);
+            if (pivotTransform.name == "GuidePivot.1")
+            {
+                curLine[0] = current;
+            }
+            if (pivotTransform.name == "GuidePivot.2")
+            {
+                curLine[1] = current;
+            }
+            if (pivotTransform.name == "GuidePivot.3")
+            {
+                curLine[2] = current;
+
+                if (oldLine[0] != null)
+                {
+                    oldLine[0].next = curLine[0];
+                    oldLine[0].right = curLine[1];
+                    curLine[0].prev = oldLine[0];
+                }
+                if (oldLine[1] != null)
+                {
+                    oldLine[1].left = curLine[0];
+                    oldLine[1].next = curLine[1];
+                    oldLine[1].right = curLine[2];
+                    curLine[1].prev = oldLine[1];
+                }
+                if (oldLine[2] != null)
+                {
+                    oldLine[2].left = curLine[1];
+                    oldLine[2].next = curLine[2];
+                    curLine[2].prev = oldLine[2];
+                }
+
+                guideLine.Add(curLine[0]);
+                guideLine.Add(curLine[1]);
+                guideLine.Add(curLine[2]);
+
+                oldLine[0] = curLine[0];
+                oldLine[1] = curLine[1];
+                oldLine[2] = curLine[2];
+            }
+        }
+        int pivotNum = guideLine.Count;
+        guideLine[0].prev = guideLine[pivotNum - 3];
+        guideLine[1].prev = guideLine[pivotNum - 2];
+        guideLine[2].prev = guideLine[pivotNum - 1];
+
+        guideLine[pivotNum - 3].next = guideLine[0];
+        guideLine[pivotNum - 3].right = guideLine[1];
+
+        guideLine[pivotNum - 2].left = guideLine[0];
+        guideLine[pivotNum - 2].next = guideLine[1];
+        guideLine[pivotNum - 2].right = guideLine[2];
+
+        guideLine[pivotNum - 1].left = guideLine[1];
+        guideLine[pivotNum - 1].next = guideLine[2];
+
+        Gizmos.color = lineColor;
         
+        foreach(GuidePivot gp in guideLine)
+        {
+            Gizmos.DrawWireSphere(gp.cur.position, 0.5f);
+            if (gp.next != null)
+                Gizmos.DrawLine(gp.cur.position, gp.next.cur.position);
+            if (gp.left != null)
+                Gizmos.DrawLine(gp.cur.position, gp.left.cur.position);
+            if (gp.right != null)
+                Gizmos.DrawLine(gp.cur.position, gp.right.cur.position);
+            
+        }
     }
 }
